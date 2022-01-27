be6588d7c464fdb6c326daed293d78c46ce4b02d
Marcos <marcosgribeiro@gmail.com>
2022-01-27 16:32:16 -0300

    fix

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE HTML>
+<!--
+	Broadcast by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>IFriendship: Sistema de Recomendação de Amizades</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="icon" href="images/as.png">
+
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<h1><a href="#">IFriendship <span>by Luis, Fran and Lucas</span></a></h1>
+				<a href="#menu">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="#2">Metodologia</a></li>
+					<li><a href="generic.html">Formulário</a></li>
+					
+					
+				</ul>
+			</nav>
+
+			<!-- Banner -->
+			<!--
+				To use a video as your background, set data-video to the name of your video without
+				its extension (eg. images/banner). Your video must be available in both .mp4 and .webm
+				formats to work correctly.
+			-->
+				<section id="banner" data-video="images/banner">
+					<div class="inner">
+						<header>
+							<h1>Bem-vindo ao IFriendship</h1>
+							<p class="pspecial">Um sistema de recomendação de amizades, pensado e desenvolvido<br />
+							pelos alunos Luis Filipe Vasconcellos, Franciele Fernandes e Lucas de Jesus, sob orientação do Professor Marcos Guerine.</p>
+						</header>
+						<a href="#main" class="button big alt scrolly">Saiba Mais</a>
+					</div>
+
+				</section>
+
+		<!-- Main -->
+			<div id="main">
+
+			<!-- One -->
+				<section class="wrapper style1">
+					<div class="inner">
+						<header class="align-center">
+							<h2>IFriendship</h2>
+
+							<p>Em um mundo cada vez mais digital, a quantidade de informações produzidas por instante de tempo
+								cresce exponencialmente. Essa disponibilidade de grandes quantidades de dados motivou o
+								crescimento de pesquisas que realizam o estudo e processamento de grandes bases de dados,
+								tentando, a todo custo, entender o comportamento dessa massa de dados, relacionando informações
+								e buscando por padrões e regras que tenham alguma relevância [1]. Redes sociais, em geral, possuem
+								como característica produzir e agrupar diversas informações à medida que relacionamentos e
+								interações sociais são criadas no meio digital. Muitas vezes, a simples conexão no meio digital não
+								implica em interação e amizade no ambiente real. Em uma comunidade escolar, a falta dessa interação
+								social é preocupante e fica ainda mais evidente. Neste contexto, o projeto IFriendship surge como uma
+								plataforma que foi desenvolvida com intuito de estreitar laços entre os alunos que possuem
+								características e interesses semelhantes, mas que, por diversos motivos, não interagem entre si.Espera-se
+								que, com a utilização da comunidade, os efeitos sejam positivos e novos relacionamentos sejam firmados.
+							</p>
+						
+							
+						</header>
+						<!-- 2 Column Video Section -->
+<hr class="major" />
+<div  class="align-center">
+Essa série de perguntas nos ajudará a encontrar novos amigos para você:<br><br>
+<a href="generic.html" class="button big alt scrolly">Responda ao questionário</a>
+	</div>						
+					</div>
+				</section>
+			<!-- Three -->
+				<section id="2" class="wrapper ">
+					<div  class="inner">
+						<header class="align-center">
+							<h2>Metodologia</h2>
+							<p>
+								A plataforma criada utiliza as informações coletadas dos próprios alunos para alimentar um modelo de
+								mineração de dados. Esse modelo é implementado por meio da técnica de agrupamento de dados,
+								também conhecida como clusterização, uma tarefa clássica de Mineração de Dados que faz o
+								agrupamento de elementos semelhantes, tendo como base seus atributos e a distância efetiva entre
+								esses elementos. Para coleta de dados, foi proposto um questionário composto por treze perguntas
+								que avaliam os desejos e interesses dos discentes. A composição desse questionário foi conduzida
+								junto a psicóloga Camila Resende (servidora e psicóloga do campus), com o objetivo de entender as
+								características que mais influenciam nos relacionamentos entre pessoas, em especial, dos alunos do
+								IFRJ – Campus Pinheiral. Com o IFriendship, almeja-se uma maior interação social entre os alunos da
+								instituição, de maneira que haja uma expansão dos círculos de amizades, encontrando pessoas que
+								compartilham de mesmos interesses e prontas para fazerem novos vínculos de amizade. O projeto foi
+								desenvolvido utilizando diversas tecnologias da área de computação: o formulário inicial foi criado
+								usando o Google Forms; a interface da página foi implementada em programação web (php, CSS e
+								html); e a técnica de clusterização, conhecida como kMeans, foi implementada em Python.
+							</p>
+						</header>
+
+						<!-- 3 Column Video Section -->
+							
+					</div>
+				</section>
+
+			</div>
+
+
+				<div class="copyright">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-snapchat"><span class="label">Snapchat</span></a></li>
+					</ul>
+					IFriendship
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
